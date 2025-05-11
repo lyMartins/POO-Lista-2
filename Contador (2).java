126fd60e7a62ead45ff85ddeba76462a548a2539
Carlyle Martins <138628247+lyMartins@users.noreply.github.com>
2025-05-10 21:25:46 -0300

    Add files via upload

---
+++ Contador (2).java
@@ -0,0 +1,25 @@
+package q1Lista2;
+
+public class Contador {
+	private int Valor;
+	
+	public int getValor() {
+		return Valor;
+	}
+
+	public void Zerar() {
+		Valor = 0;
+	}
+
+	public void setValor(int Valor) {
+		this.Valor = Valor;
+	}
+	
+	public void incrementar() {
+		this.Valor++;
+	}
+
+
+}
+
+
